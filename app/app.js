d99bcd94e89ad7338a1d54e34acc9f6451465cc3
Felix Leupold <mail@felixleupold.de>
2015-08-28 22:29:12 +0200

    working validation

--- app/app.js
+++ app/app.js
@@ -6,13 +6,15 @@ console.log('-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 //require modules
 var path = require('path');
 var express = require('express');
+var expressValidator = require('express-validator');
+var bodyParser = require('body-parser');
 
 //init
 var app = express();
 var exphbs  = require('express-handlebars');
 
 //add templating engine
-app.engine('hbs', 
+app.engine('hbs',
 	exphbs({
 		defaultLayout: 'default',
 		extname: '.hbs'
@@ -20,6 +22,10 @@ app.engine('hbs',
 );
 app.set('views', __dirname + '/views/pages');
 app.set('view engine', 'hbs');
+
+//parse parameters out of (post) requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 
 //serve resources statics
 app.use( express.static( path.join(__dirname, '../prototype/_output') ) );
@@ -31,11 +37,42 @@ app.get('/', function(req, res){
 
 //contact form
 app.get('/contactform', function(req, res){
+	console.log('get', req.body);
 	res.render('contactform', {});
 })
 
 app.post('/contactform', function(req, res){
-	res.render('contactform', {});
+	var fields = req.body || {};
+	console.log('post', req.body);
+
+	console.log(fields.message);
+
+	//sanitize
+	req.sanitize('name').stripLow(true);
+	req.sanitize('name').trim();
+	req.sanitize('email').stripLow(true);
+	req.sanitize('email').trim();
+	req.sanitize('message').stripLow(true);
+	req.sanitize('message').trim();
+
+	//check fields
+	req.assert('email', 'valid email address required').isEmail();
+	req.assert('email', 'please provide your email address').notEmpty();
+	req.assert('name', 'please provide your name').notEmpty();
+	req.assert('message', 'please write a message').notEmpty();
+
+	// var errors = req.validationErrors();
+	var errors = req.validationErrors(true); //with true = mapped
+
+	console.log(fields.message + "");
+
+	console.log('errors:', errors);
+	
+	res.render('contactform', {
+		fields: fields,
+		errors: errors
+		// ,layout: false
+	});
 })
 
 // start server
