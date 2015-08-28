dbb5955586d12c1259759c358178dba3dc6ede0b
Felix Leupold <mail@felixleupold.de>
2015-08-28 14:03:14 +0200

    added node server

---
+++ app/app.js
@@ -0,0 +1,22 @@
+// xiel development
+console.log('-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -');
+console.log('-  -  -  -  -  -     RESTART     -  -  -  -  -  -  -  -  -  -  -  -  -');
+console.log('-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -');
+
+//require modules
+var express = require('express');
+
+//init
+var app = express();
+
+//route
+app.get('/', function(req, res){
+	res.send('...');
+})
+
+// start server
+var server = app.listen(62375, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at port: %s', port);
+});
