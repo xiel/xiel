71dc27f529919bc036e956c00104022ccc897980
Felix Leupold <mail@felixleupold.de>
2015-08-28 14:42:13 +0200

    move templates to app

--- app/app.js
+++ app/app.js
@@ -8,10 +8,23 @@ var express = require('express');
 
 //init
 var app = express();
+var exphbs  = require('express-handlebars');
+
+//add templating engine
+app.engine('hbs', 
+	exphbs({
+		// layoutsDir: __dirname + '/views/layouts',
+		// partialsDir: __dirname + '/views/partials',
+		defaultLayout: 'default',
+		extname: '.hbs'
+	})
+);
+app.set('views', __dirname + '/views/pages');
+app.set('view engine', 'hbs');
 
 //route
 app.get('/', function(req, res){
-	res.send('...');
+	res.render('index', {});
 })
 
 // start server
