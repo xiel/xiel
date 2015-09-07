fa154ceae771fb91daa891fac06307dac20a1344
Felix Leupold <mail@felixleupold.de>
2015-09-07 19:20:41 +0200

    minify html

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var minify = require('html-minifier').minify;
 
 router.get('/', function(req, res){
-	res.render('index', {});
+	res.render('index', {}, function(err, html){
+		if(err) return console.error(err);
+
+		var minifiedHTML = minify(html, {
+			collapseWhitespace: true
+		});
+
+		return res.send(minifiedHTML);
+	});
 });
 
 module.exports = router;
