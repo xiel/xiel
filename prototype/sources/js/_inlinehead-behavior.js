d6e82cea1a0b834a0e1efe44a9fb218874b5b2be
xiel <mail@felixleupold.de>
2016-02-12 13:12:12 +0100

    async stylesheet loading

--- prototype/sources/js/_inlinehead-behavior.js
+++ prototype/sources/js/_inlinehead-behavior.js
@@ -3,6 +3,7 @@
     var document = window.document;
     var ASSETBASEPATH = window.siteData && siteData.basePath || '';
     var docElem = document.documentElement;
+    var asyncStylesheets = window.siteData && window.siteData.asyncStylesheets;
     var loadJs = function( src, ordered, cb ){
         var script = document.createElement( "script" );
 
@@ -15,46 +16,49 @@
         document.head.appendChild(script);
         return script;
     };
+
     /* if you need async CSS */
-    //var loadCss = function( href, media, callback ){
-    //    var timer;
-    //    var ss = document.createElement('link');
-    //    var sheets = document.styleSheets;
-    //
-    //    var onLoad = function(){
-    //        setTimeout(onloadcssdefined);
-    //        ss.removeEventListener('load', onLoad);
-    //    };
-    //    var onloadcssdefined = function(){
-    //        var resolvedHref = ss.href;
-    //        var i = sheets.length;
-    //        while( i-- ){
-    //            if( sheets[ i ].href == resolvedHref ){
-    //                ss.media = media;
-    //                if(callback){
-    //                    callback();
-    //                }
-    //                clearInterval(timer);
-    //                break;
-    //            }
-    //        }
-    //    };
-    //
-    //    media = media || 'all';
-    //
-    //    ss.rel = 'stylesheet';
-    //
-    //    timer = setInterval(onloadcssdefined, 19);
-    //    ss.addEventListener('load', onLoad);
-    //
-    //    ss.href = href;
-    //
-    //    ss.media = 'x';
-    //
-    //    document.head.appendChild(ss);
-    //
-    //    return ss;
-    //};
+    var loadCss = function( href, media, callback ){
+       var timer;
+       var ss = document.createElement('link');
+       var sheets = document.styleSheets;
+    
+       var onLoad = function(){
+           setTimeout(onloadcssdefined);
+           ss.removeEventListener('load', onLoad);
+       };
+       var onloadcssdefined = function(){
+           var resolvedHref = ss.href;
+           var i = sheets.length;
+           while( i-- ){
+               if( sheets[ i ].href == resolvedHref ){
+                   ss.media = media;
+                   if(callback){
+                       callback();
+                   }
+                   clearInterval(timer);
+                   break;
+               }
+           }
+       };
+    
+       media = media || 'all';
+    
+       ss.rel = 'stylesheet';
+    
+       timer = setInterval(onloadcssdefined, 19);
+       ss.addEventListener('load', onLoad);
+    
+       ss.href = href;
+    
+       ss.media = 'x';
+
+       console.log(href, ss);
+    
+       document.head.appendChild(ss);
+    
+       return ss;
+    };
 
     docElem.classList.remove('no-js');
     docElem.classList.add('js');
@@ -66,13 +70,24 @@
         loadJs(ASSETBASEPATH + 'js/_main-behavior.js', true);
     });
 
+    setTimeout(function(){
+        if(asyncStylesheets){
+            asyncStylesheets.forEach(function(asyncStylesheet){
+                if(typeof asyncStylesheet === 'string'){
+                    loadCss(asyncStylesheet);
+                } else {
+                    loadCss(asyncStylesheet.href, asyncStylesheet.media, asyncStylesheet.callback);
+                }
+            });
+        }
+    });
 
     //uncomment if you have crucial fonts placed above inline script
-    //if (document.fonts && document.fonts.forEach) {
-    //    setTimeout(function () {
-    //        document.fonts.forEach(function(font){
-    //            font.load();
-    //        });
-    //    }, 200);
-    //}
+    if (document.fonts && document.fonts.forEach) {
+       setTimeout(function () {
+           document.fonts.forEach(function(font){
+               font.load();
+           });
+       });
+    }
 })(window);
