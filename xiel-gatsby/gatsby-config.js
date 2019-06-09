b1f30bd82336ff4ad776b95136521bb1d0fb6d07
Felix Leupold <felix@xiel.de>
2019-06-09 19:20:13 +0200

    update title

--- xiel-gatsby/gatsby-config.js
+++ xiel-gatsby/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `XIEL`,
+    title: `FrontEnd Developer · Felix Leupold · XIEL`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
--- xiel-gatsby/src/pages/page-2.tsx
+++ xiel-gatsby/src/pages/page-2.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { graphql, Link, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
