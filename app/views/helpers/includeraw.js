822fb5d03fdd4a132a0ed3d23040ca521bcd052f
Felix Leupold <felix@xiel.de>
2019-07-21 19:41:45 +0200

    delete old files, move new project up

--- app/views/helpers/includeraw.js
+++
@@ -1,7 +0,0 @@
-module.exports.register = function (Handlebars, options) {
-    var grunt = require('grunt');
-
-    Handlebars.registerHelper('includeraw', function(src){
-        return new Handlebars.SafeString(grunt.file.read(src));
-    });
-};
--- prototype/grunt/configs/qunit.js
+++
@@ -1,8 +0,0 @@
-/**
- * Configuration QUnit
- *
- * {@link} https://github.com/gruntjs/grunt-contrib-qunit/
- */
-module.exports = {
-	files: 'tests/*.html',
-};
