ee74c46ef9de1f2e20001c53b4989e9f3c08f7c4
Felix Leupold <felix@xiel.de>
2019-09-07 01:01:30 +0200

    ...

--- src/pageTemplates/index.tsx
+++ src/pageTemplates/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Layout from '../components/Layout/Layout'
 import SEO from '../components/seo'
 import { PageProps } from '../types/PageProps'
-import { useTranslation } from 'react-i18next'
 import CorePrinciples from '../components/Home/CorePrinciples'
 import StageSection from '../components/Home/Stage/StageSection'
 import ClientsPartners from '../components/Home/ClientsPartners'
@@ -10,8 +9,6 @@ import IntroSection from '../components/Home/IntroSection'
 import Technologies from '../components/Home/Technologies'
 
 export default function Index(props: PageProps) {
-  const { t } = useTranslation()
-
   return (
     <Layout>
       <SEO />
