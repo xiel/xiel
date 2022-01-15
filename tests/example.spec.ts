6e4048b7331f6ecc959257158e932f7ef1bc9e77
Felix Leupold <felix@xiel.de>
2022-01-15 17:15:32 +0100

    remove snapshots

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,7 @@
-import { expect, test } from './utils/testFixtures'
+import { test } from './utils/testFixtures'
 
 test('basic test', async ({ page, queries: q }) => {
   await page.goto('/')
   await q.findByRole('heading', { name: 'Felix Leupold' })
   await q.findByRole('heading', { name: 'Clients & Partners' })
-
-  expect(await page.screenshot()).toMatchSnapshot('landing.png')
-})
-
-test('visual test', async ({ page }) => {
-  await page.goto('/')
-  expect(await page.screenshot()).toMatchSnapshot('landing.png')
-})
-
-test('visual test - de', async ({ page }) => {
-  await page.goto('/de')
-  expect(await page.screenshot()).toMatchSnapshot('landing-de.png')
 })
