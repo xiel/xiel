409ba9a04d61d0f426eafa07be90be6026309c98
Felix Leupold <felix@xiel.de>
2022-01-15 14:24:42 +0100

    add @playwright-testing-library/test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,7 @@
-import { test } from '@playwright/test'
+import { test } from './utils/testFixtures'
 
-test('basic test', async ({ page }) => {
+test('basic test', async ({ page, queries: q }) => {
   await page.goto('/')
+  await q.findByRole('heading', { name: 'Felix Leupold' })
+  await q.findByRole('heading', { name: 'Clients & Partners' })
 })
---
+++ tests/utils/testFixtures.ts
@@ -0,0 +1,15 @@
+import { test as baseTest } from '@playwright/test'
+import {
+  fixtures,
+  TestingLibraryFixtures,
+} from '@playwright-testing-library/test/fixture'
+
+// Alternatively, with other fixtures
+interface Fixtures extends TestingLibraryFixtures {
+  // ... additional fixture types
+}
+
+export const test = baseTest.extend<Fixtures>({
+  ...fixtures,
+  // ... additional fixtures
+})
